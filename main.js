1bd9af3e26bae8d37a67405fe4e63333904e41c4
Ankit <ankit0839483@gmail.com>
2024-10-11 23:41:11 +0530

    add 2nd term

--- main.js
+++ main.js
@@ -62,4 +62,42 @@ function setPiece(){
     r-=1;//updating
     currColumns[c]=r;
 
+    checkWinner();
+
+}
+function checkWinner(){
+    //horizontally
+    for (let r=0;r<rows;r++){
+        for (let c=0;c<columns-3;c++){
+            if(board[r][c]!=''){
+                if(board[r][c]==board[r][c+1]&&board[r][c+1] == board[r][c+2]&&board[r][c+2]==board[r][c+3]){
+                    setWinner(r,c);
+                    return;
+                }
+            }
+        }
+    }
+
+    //vertically
+    for (let c=0;c<columns;c++){
+        for (let r=0;r<rows-3;r++){
+            if(board[r][c]!=''){
+                if(board[r][c]==board[r+1][c]&&board[r+1][c] == board[r+2][c]&&board[r+2][c]==board[r+3][c]){
+                    setWinner(r,c);
+                    return;
+                }
+            }
+        }
+    }
+
+}
+function setWinner(r,c){
+    let winner=document.getElementById("winner");
+    if(board[r][c]==playerRed){
+        winner.innerText = "Red Wins";
+    }
+    else{
+        winner.innerText= "Yellow Wins";
+    }
+    gameOver = true;
 }
